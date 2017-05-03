449839f693ce9ab4b58611fd94859916b77ddb04
elin.angelov <elin.angelov@softwaregroup-bg.com>
2017-05-03 16:36:29 +0300

    fix: emacs moved

---
+++ home/.emacs.d/init.el
@@ -0,0 +1,25 @@
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
+(load-theme 'foggy-night t)
+(global-linum-mode t)
+(global-hl-line-mode t)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(nyan-mode t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (nyan-mode foggy-night-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
