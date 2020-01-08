58aea5e66699d5cdab55d052e0176799391b9d3e
Elin Angelov <zetxx@belogradchik.biz>
2020-01-08 14:22:05 +0200

    Delete env.specific.el

--- home/.emacs.d/customizations/env.specific.el
+++
@@ -1,4 +0,0 @@
-(defvar ternjsPath "path to ternjs bin, usualy @ ~/npm/lib/node_modules/tern/emacs/")
-(defvar globalFont "xos4 Terminus 10")
-(defvar esLintPath "path to eslint rc file")
-(defvar tabWidth 2)
