23447f65786e306f5616760d77877ed13f4d8a28
Elin Angelow <elin.angelov@shelly.com>
2024-11-20 13:47:58 +0200

    fix: nvim

--- home/.config/nvim/after/plugin/treesitter.lua
+++ home/.config/nvim/after/plugin/treesitter.lua
@@ -1,9 +1,12 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "markdown", "markdown_inline", "go", "typescript", "dockerfile", "ssh_config", "sql", "json", "jsdoc", "javascript", "lua", "vim" },
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+    ensure_installed = { "markdown", "markdown_inline", "go", "typescript", "dockerfile", "ssh_config", "sql", "json", "jsdoc", "lua", "vim", "javascript" },
+    sync_install = false,
+    auto_install = true,
+    indent = {
+        enable = true
+    },
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
 }
--- home/.config/nvim/lua/zetxx/remap.lua
+++ home/.config/nvim/lua/zetxx/remap.lua
@@ -1,3 +1,7 @@
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
