02d969cd0f47f3931e66e5bdec19e2a73a3eebe3
Elin Angelow <elin.angelov@shelly.com>
2024-11-26 09:31:58 +0200

    fix: telescope list buffers

--- home/.config/nvim/after/plugin/telescope.lua
+++ home/.config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,7 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find in git files' })
+vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = 'Telescope list buffers' })
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input('Grep >') })
 end, { desc = 'Telescope project serch' })
