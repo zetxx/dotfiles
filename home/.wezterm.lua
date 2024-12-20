56385bfe3682e32f62f64745eb2be76c2a6aefd0
zetxx <zetxx@belogradchik.biz>
2024-12-20 14:36:10 +0200

    fix: rf

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -3,7 +3,7 @@ local config = wezterm.config_builder()
 
 config.font = wezterm.font('Ubuntu Mono', { weight = 'Regular' })
 config.font_size = 12
-config.max_fps = 75
-config.animation_fps = 75
+config.max_fps = 60
+config.animation_fps = 60
 
 return config
