30ba53dc83e2ce137cea36f656ba9ed1eeebd9ce
Elin Angelow <elin.angelov@shelly.com>
2025-12-29 17:28:22 +0200

    fix: nvim

--- home/.config/nvim/after/plugin/treesitter.lua
+++ home/.config/nvim/after/plugin/treesitter.lua
@@ -1,19 +1,20 @@
-require 'nvim-treesitter.configs'.setup {
-    ensure_installed = { "markdown", "markdown_inline", "go", "typescript", "dockerfile", "ssh_config", "sql", "json", "jsdoc", "lua", "vim", "javascript", "rust" },
-    sync_install = false,
-    auto_install = true,
-    indent = {
-        enable = true
-    },
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-    },
-    refactor = {
-        highlight_current_scope = { enable = true },
-        highlight_definitions = {
-            enable = true,
-            clear_on_cursor_move = false,
-        },
-    },
-}
+-- require 'nvim-treesitter.configs'.setup {
+--     ensure_installed = { "markdown", "markdown_inline", "go", "typescript", "dockerfile", "ssh_config", "sql", "json", "jsdoc", "lua", "vim", "javascript", "rust" },
+--     sync_install = false,
+--     auto_install = true,
+--     indent = {
+--         enable = true
+--     },
+--     highlight = {
+--         enable = true,
+--         additional_vim_regex_highlighting = false,
+--     },
+--     refactor = {
+--         highlight_current_scope = { enable = true },
+--         highlight_definitions = {
+--             enable = true,
+--             clear_on_cursor_move = false,
+--         },
+--     },
+-- }
+require'nvim-treesitter'.install { 'markdown', 'markdown_inline', 'go', 'typescript', 'dockerfile', 'ssh_config', 'sql', 'json', 'jsdoc', 'lua', 'vim', 'javascript', 'rust' }
